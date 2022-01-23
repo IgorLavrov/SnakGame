d457f051dd4f9161f2581ec0b9456e36cdad6f5c
Lavrovi <igor.lavrov90@hotmail.co.uk>
2022-01-23 22:14:15 +0200

    commit

---
+++ SnakGame/Player.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakGame
+{
+  public  class Player
+ 
+        {
+            string fam;     // поле фамилия
+            string status;  // поле статус
+            int score;        // поле возраст 
+
+            public Player() { }
+            public Player(string _fam)
+            {
+                fam = _fam;
+            }
+            public string Fam  //свойство для поля фамилия
+            {
+                set { if (fam == null) fam = value; }     //код - установка значения поля фамилия
+                get { return fam; }                     //код - чтение значения поля  фамилия
+            }
+            public int Score        //свойство для поля возраст
+            {
+                set                    //код - установка значения полей возраст и статус
+                {
+                    score = value;
+                    if (score < 2) status = " Child";
+                    else if (score < 7) status = "Scholar";
+                    else if (score < 12) status = " Mature";
+                    else status = "Expert";
+
+                }
+                get { return (score); }       //код - чтение значения поля  возраст
+            }
+            public string Status  //свойство для поля статус
+            {
+                get { return status; }    // код - чтение поля статус
+            }
+          
+            public void Show()  // метод вывода значений полей
+            {
+                Console.WriteLine("Surname " + fam);
+                Console.WriteLine("Score " + score);
+                Console.WriteLine("Status " + status);
+
+
+            }
+        }
+
+    }
+
+
+
--- SnakGame/Program.cs
+++ SnakGame/Program.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using System.IO;
+using System.Windows;
 
 namespace SnakGame
 {
@@ -12,55 +14,137 @@ namespace SnakGame
         static void Main(string[] args)
         {
             Console.WriteLine(Console.LargestWindowHeight);
-          
-
             Walls walls = new Walls(80, 25);
-            walls.Draw();
-
-
 
             Point p = new Point(4, 5, '*');
             Snake snake = new Snake(p, 4, Direction.RIGHT);
-            snake.Drow();
-            int count = 0 ;
 
-            FoodCreator foodCreator = new FoodCreator(60, 15, '$');
+            FoodCreator foodCreator = new FoodCreator(40, 10, '$');
             Point food = foodCreator.CreateFood();
-            food.draw();
 
-            while (true)
+            int count = 0;
+            int n = 0;
+            String s, subkey, key;
+
+            Player[] pr = new Player[100] ;          
+            for (int i = 0; i < pr.Length; i++)
+            { pr[i] = new Player(); }
+            do                               
             {
-                if (walls.IsHit(snake) || snake.isHitTail())
+                Console.WriteLine("***Menu***");
+                Console.WriteLine("1 - Make  a record ");
+                Console.WriteLine("2 - Show Result");
+                Console.WriteLine("3 - Play");
+                Console.WriteLine("4 - Exit");
+                Console.WriteLine();
+                Console.WriteLine(" Your Choose...");
+                key = Console.ReadLine();
+                switch (key)
                 {
-                    break;
-                }
+                    case "1":
+                            Console.WriteLine("Enter your Surname and Name: ");
+                            s = Console.ReadLine(); pr[n].Fam = s;
+                            pr[n].Score = count;
+                            n++;
+                        break;
+                     
+                    case "2":               
+                        for (int i = 0; i < n; i++)
+                        {
+                            pr[i].Show();
+                            Console.WriteLine
+                                ();
+                        }
+                        break;
 
-                if (snake.Eat(food))
-                {
-                    count++;
-                    food = foodCreator.CreateFood();
-                    food.draw();
-                }
-                else
-                {
-                    snake.Move();
-                }
-                Thread.Sleep(100);
+                    case "3":
+                        Console.Clear();
+                        walls.Draw();
+                        snake.Drow();
+                        food.draw();
+                            while (true)
+                            {
+                                if (walls.IsHit(snake) || snake.isHitTail())
+                                {
+                                    break;
+                                }
 
-                if (Console.KeyAvailable)
-                {
-                    ConsoleKeyInfo key = Console.ReadKey();
-                    snake.HandleKey(key.Key);
+                                if (snake.Eat(food))
+                                {
+                                    count++;
+                                    food = foodCreator.CreateFood();
+                                    food.draw();
+                                }
+                                else
+                                {
+                                    snake.Move();
+                                }
+                                Thread.Sleep(100);
 
-                }
+                                if (Console.KeyAvailable)
+                                {
+                                    ConsoleKeyInfo key1 = Console.ReadKey();
+                                    snake.HandleKey(key1.Key);
 
+                                }
+
+                            }
+                            GameOver snake2 = new GameOver();
+                            snake2.EndOfGame(count);
+                            Console.ReadLine();
+
+                            break;
+                        }
+                
+                } while (key != "4") ;
+
+                /*
+               Console.WriteLine(Console.LargestWindowHeight);
+
+
+                   Walls walls = new Walls(80, 25);
+                   walls.Draw();
+
+
+
+                   Point p = new Point(4, 5, '*');
+                   Snake snake = new Snake(p, 4, Direction.RIGHT);
+                   snake.Drow();
+                   int count = 0 ;
+
+                   FoodCreator foodCreator = new FoodCreator(60, 15, '$');
+                   Point food = foodCreator.CreateFood();
+                   food.draw();
+
+                   while (true)
+                   {
+                       if (walls.IsHit(snake) || snake.isHitTail())
+                       {
+                           break;
+                       }
+
+                       if (snake.Eat(food))
+                       {
+                           count++;
+                           food = foodCreator.CreateFood();
+                           food.draw();
+                       }
+                       else
+                       {
+                           snake.Move();
+                       }
+                       Thread.Sleep(100);
+
+                       if (Console.KeyAvailable)
+                       {
+                           ConsoleKeyInfo key = Console.ReadKey();
+                           snake.HandleKey(key.Key);
+
+                       }
+
+                   }*/
             }
 
-            GameOver snake2 = new GameOver();
-            snake2.EndOfGame(count);
-            Console.ReadLine();
-        }
-
-       
+       } 
     }
-}
+
